87c3013b2a7f0872ef00934bd0dbe0794697f48c
Josuehmj <josueahm16gmail.com>
2026-06-13 17:33:00 -0600

    corrección de error de sintaxis en apb_driver

--- tb/agents/apb/apb_driver.sv
+++ tb/agents/apb/apb_driver.sv
@@ -74,7 +74,7 @@ class apb_driver extends uvm_driver #(apb_seq_item);
     repeat (item.idle_cycles) @(vif.driver_cb);
 
     `uvm_info("APB_DRV_DBG",
-      $sformatf(("NUEVA TXN: addr=0x%04h write=%0b wdata=0x%08h",
+      $sformatf("NUEVA TXN: addr=0x%04h write=%0b wdata=0x%08h",
                 item.addr, item.write, item.wdata), UVM_NONE)
 
     // 2. SETUP — 1 ciclo con psel=1, penable=0
